f216bda43d1c52c418100947d632d02985abfc5c
Travis Exline <t.exline22@gmail.com>
2019-08-13 14:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  for (i = 0, i < names.length, i++) {
+  for (let i = 0; i < names.length; i++) {
     console.log(`Thank you, #{names[i]}, for the wonderful #{event} gift`)
   }
   return names
